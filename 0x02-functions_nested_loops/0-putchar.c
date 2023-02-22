72bf59ea56aadd30c0e943f39611852190afe7a5
HezdaneNaxlube254 <hezronzid81@gmail.com>
2023-02-22 07:28:51 -0800

    ./2-alphabet_x10

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
 #include "main.h"
-
 int main(void)
-{
-	int a;
-	char str[]="_putchar";
 
-	for(a=0;str[a] !='0';a++)
-		_putchar(str[a]);
+{
+	_putchar('_p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
