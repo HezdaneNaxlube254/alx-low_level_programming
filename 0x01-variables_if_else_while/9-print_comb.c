8c2f850ac24060a878a29ed24846e6564904c36c
HezdaneNaxlube254 <hezronzid81@gmail.com>
2023-05-20 01:58:38 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
+/*
+	Write a program that prints all possible com
+	binations of single-digit numbers.
+
+	Numbers must be separated by ,, followed by 
+	a space
+	Numbers should be printed in ascending order
+	You can only use the putchar function (every
+       	other function (printf, puts, etc…) is forbi
+	dden)
+	All your code should be in the main function
+	You can only use putchar four times maximum 
+	in your code
+	You are not allowed to use any variable of t
+	ype char
+
+*/
+
 int main(void)
+
 {							int num;						for (num = 0; num <= 9; num++)
 	{							putchar((num % 10) + '0');
 		if (num == 9)
+
 		{
 			continue;
 		}
+
 		putchar(',');
 		putchar(' ');
+
 	}
 
 	putchar('\n');
