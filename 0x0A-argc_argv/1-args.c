85e3e29a004e8a8af43f6968e6f8536356d464e5
HezdaneNaxlube254 <hezronzid81@gmail.com>
2023-10-31 03:27:42 -0700

    command printing number of arguements passed to it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,7 @@
+/*aprogram to print arguements passed to it@nashkay=10/31/2023*/
+#include<stdio.h>
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n",argc-1);
+	return 0;
+}
